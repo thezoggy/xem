621679d24777fac94aa01cf86e49187d7c098f21
Dennis Lutter <lad1337@gmail.com>
2011-11-09 20:37:04 +0100

    whitespace

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -5,7 +5,7 @@ class Api extends CI_Controller {
 
 	function __construct(){
 		parent::__construct();
-		
+
 		$this->load->helper("url");
 		$this->load->model('dbobjectcache');
 		$this->history = new History($this->db,$this->session);
@@ -15,27 +15,27 @@ class Api extends CI_Controller {
 	public function index(){
 		redirect('xem');
 	}
-	
-	
+
+
 	public function saveCon(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
-		
+		}
+
 		/*
 		Reg successful for: ?cmd=newCon&fromLiID=tvdb_1_5&toLiID=anidb_1_3&conID=3&paperID=paper_tvdb_anidb&fID=2&fName=tvdb&fSeason=1&fEpisode=1&tID=3&tName=anidb&tEpisode=3
 		*/
 		$origin = new Location($this->oh, $_POST['fID']);
 		$destination = new Location($this->oh, $_POST['tID']);
-		
+
 		$directrule = new Directrule($this->oh);
 		array('origin_id','destination_id', 'element_id', 'name_id', 'origin_season', 'origin_episode', 'destination_season',	'destination_episode');
 		$directrule->origin_id = $_POST['fID'];
 		$directrule->destination_id = $_POST['tID'];
 		$directrule->element_id = $_POST['elementID'];
 		$directrule->name_id = null;
-		
+
 		$directrule->origin_season = $_POST['fSeason'];
 		$directrule->origin_episode = $_POST['fEpisode'];
 		$directrule->destination_season = $_POST['tSeason'];
@@ -46,45 +46,45 @@ class Api extends CI_Controller {
 			$directrule->save();
 			if($directrule->id){
 				$this->_fullOut('success',$_POST);
-				return true;		
+				return true;
 			}
 		}
 		$this->_fullOut('failure',$_POST);
-		
-		
+
+
 	}
 	public function deleteCon(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$directrule = new Directrule($this->oh);
 		$directrule->origin_id = $_POST['fID'];
 		$directrule->destination_id = $_POST['tID'];
 		$directrule->element_id = $_POST['elementID'];
 		$directrule->name_id = null;
-		
+
 		$directrule->origin_season = $_POST['fSeason'];
 		$directrule->origin_episode = $_POST['fEpisode'];
 		$directrule->destination_season = $_POST['tSeason'];
 		$directrule->destination_episode = $_POST['tEpisode'];
-		
-		
+
+
 		if($directrule->delete()){
 			$directrule->reverse();
 			if($directrule->delete()){
 				$this->_fullOut('success',$_POST);
-				return true;	
+				return true;
 			}
 		}
 		$this->_fullOut('failure',$_POST);
 	}
-	
+
 	public function savePassthru(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$orign = new Location($this->oh);
 		$orign->name = $_POST['origin'];
 		$orign->load();
@@ -92,7 +92,7 @@ class Api extends CI_Controller {
 		$destination->name = $_POST['destination'];
 		$destination->load();
 		if($orign->id && $destination->id){
-			
+
 			$newPass = new Passthru($this->oh, (int)$_POST['id']);
 			$newPass->origin_id = $orign->id;
 			$newPass->destination_id = $destination->id;
@@ -110,89 +110,89 @@ class Api extends CI_Controller {
 				}
 			}
 		}
-		
-		$this->_fullOut('failure',$_POST);	
+
+		$this->_fullOut('failure',$_POST);
 	}
-	
+
 	public function deletePassthru(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
-		
+		}
+
 		$orign = new Location($this->oh);
 		$orign->name = $_POST['origin'];
 		$orign->load();
 		$destination = new Location($this->oh);
 		$destination->name = $_POST['destination'];
 		$destination->load();
-		
+
 		if($orign->id && $destination->id){
 			$newPass = new Passthru($this->oh);
-			
+
 			$newPass->origin_id = $orign->id;
 			$newPass->destination_id = $destination->id;
 			$newPass->element_id = $_POST['element_id'];
-			
+
 			if($newPass->delete()){
-				
+
 				$newPass->reverse();
 				if($newPass->delete()){
 					$this->_fullOut('success',$_POST);
 					return true;
 				}
 			}
-		
-			
+
+
 		}
-		
-		json_encode($this->_fullOut('failure',$_POST));	
+
+		json_encode($this->_fullOut('failure',$_POST));
 	}
 	function saveEntityOrder(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$element = new Element($this->oh, $_POST['element_id']);
 		$element->entity_order = $_POST['order'];
 		$element->save();
-		
-		$this->_fullOut('success',$_POST);	
-		
+
+		$this->_fullOut('success',$_POST);
+
 	}
-	
+
 	function saveNewName(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$element = new Element($this->oh, $_POST['element_id']);
 		$element->main_name = $_POST['name'];
 		$element->save();
-		
+
 		$this->_fullOut('success',$_POST);
 	}
-	
+
 	function saveAltenativeName(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$name = new Name($this->oh, $_POST['name_id']);
 		$name->name = $_POST['name'];
 		$name->save();
-		
-		$this->_fullOut('success',$_POST);	
+
+		$this->_fullOut('success',$_POST);
 	}
 
 	function deleteAltenativeName(){
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$name = new Name($this->oh, $_POST['name_id']);
 		$name->delete();
-		
+
 		$this->_fullOut('success',$_POST);
 	}
 
@@ -200,11 +200,11 @@ class Api extends CI_Controller {
 		if(!$this->session->userdata('logged_in')) {
 			$this->_fullOut('failure',array(),"not loged in");
 			return false;
-		} 
+		}
 		$out = array();
-		
+
 		$obj_id = $_POST['element_id'];
-		
+
 		$result = $this->db->query("SELECT * FROM `history` WHERE `element_id` = '".$obj_id."' ORDER BY `time` DESC");
 		if(rows($result)){
 			foreach($result->result() as $curRevsion){
@@ -225,8 +225,8 @@ class Api extends CI_Controller {
 					$old = $oldRaw;
 					$new = $newRaw;
 				}
-									
-				
+
+
 				$userName = userNameByID($this->db,$curRevsion->user_id); // this might make ist very slow
 				$out[] = array("time"=>$curRevsion->time,
 								"revision"=>$curRevsion->revision,
@@ -238,10 +238,10 @@ class Api extends CI_Controller {
 								"new"=>json_encode($new));
 			}
 		}
-		
+
 		$this->_fullOut('success',$out);
 	}
-	
+
 	function autocomplete(){
 		if(!isset($_POST['term'])){
 			$this->_fullOut('failure',array(),'no search term');
@@ -255,16 +255,16 @@ class Api extends CI_Controller {
 		if(rows($tmp))
 			foreach($tmp->result() as $curRow){
 				array_push($out,$curRow->main_name);
-			}		
+			}
 		$tmp = $this->db->query('SELECT name FROM `names` WHERE (`name` LIKE "%'.$term.'%") AND `status` > 0 ORDER BY `name`');
 		if(rows($tmp))
 			foreach($tmp->result() as $curRow){
 				array_push($out,$curRow->name);
 			}
-		
+
 		$this->_fullOut('success',$out);
 		return;
-		
+
 	}
 
 	function _buildSimpleLanguageArray(){
@@ -276,20 +276,20 @@ class Api extends CI_Controller {
 	    }
 	    return $out;
     }
-    
+
 	function getLanguagesForSelect(){
 	   $out = $this->_buildSimpleLanguageArray();
 	   $out['selected'] = 'gb';
-	   $this->output->set_content_type('application/json')->set_output(json_encode($out)); 
+	   $this->output->set_content_type('application/json')->set_output(json_encode($out));
     }
-	
+
 	function getLanguages(){
 	    $langs = $this->db->get('languages');
 	    // i will let this crash when no languages are set a check does not make sence in running env
 	    $out = $langs->result_array();
 		$this->_fullOut('success',$out);
     }
-	
+
     function nameUpdate(){
         $name = new Name($this->oh, $_POST['name_id']);
         if(isset($_POST['name']))
@@ -297,12 +297,12 @@ class Api extends CI_Controller {
         if(isset($_POST['language']))
             $name->language = $_POST['language'];
         $name->save();
-        
+
 		$this->_fullOut('success',$_POST);
-            
+
     }
-    
-    
+
+
     /*
     {RESULT_SUCCESS:"success",
         RESULT_FAILURE:"failure",
@@ -317,13 +317,13 @@ class Api extends CI_Controller {
 			$data=array();
 		$this->output->set_content_type('application/json')->set_output(json_encode(array('result'=>$status,'data'=>$data,'message'=>$message)));
 	}
-	
-	
-	
+
+
+
 	//old
 	function episodeAdress(){
 		$out = array();
-		
+
 		$show = $_GET['show'];
 		$originName = $_GET['origin'];
 		$destinationName = $_GET['destination'];
@@ -351,9 +351,9 @@ class Api extends CI_Controller {
 			if(!isset($element[0]))
 				throw new Exception("no show");
 			$elementID = $element[0]->element_id;
-			
+
 			$sRuleID = 0;
-			
+
 			$out['names'] = array();
 			$seNames = $this->db->get_where("names",array("element"=>$elementID));
 			if($seNames)
@@ -369,11 +369,11 @@ class Api extends CI_Controller {
 			if(!isset($rule_map[0]))
 				throw new Exception("no rule for origin destination combination for thsi show");
 			$rule_mapID = $rule_map[0]->id;
-			
+
 			$rules = $this->db->get_where("offset-rule",array("rule_map"=>$rule_mapID));
 			if($rules){
 				if($rules->num_rows()){
-					$rule = $this->_getBestRule($rules->result(), $oSeason, $oEpisode);	
+					$rule = $this->_getBestRule($rules->result(), $oSeason, $oEpisode);
 				}
 			}
 			if(!isset($rule)||!$rule){
@@ -382,15 +382,15 @@ class Api extends CI_Controller {
 			}
 			$out['season'] = $rule->sOff + $oSeason;
 			$out['episode'] = $rule->eOff + $oEpisode;
-			
-			
+
+
 		} catch (Exception $e) {
 		    echo 'Exception abgefangen: ',  $e->getMessage(), "\n";
-		}	
+		}
 
 		print json_encode($out);
-		
-		
+
+
 	}
 	function sceneExcpetions(){
 		$locations = $this->db->get('location');
@@ -400,9 +400,9 @@ class Api extends CI_Controller {
 				$tvdb_id = $loc->id;
 			}
 		}
-		
+
 		$this->output->set_header("Content-Type: text/plain charset=UTF-8\r\n");
-		
+
 		$el_loc_map = $this->db->get_where('element-map',array("location_id"=>$tvdb_id));
 		foreach($el_loc_map->result() as $curMap){
 			$names = $this->db->get_where('names',array('element'=>$curMap->element_id));
@@ -412,7 +412,7 @@ class Api extends CI_Controller {
 					foreach($names->result() as $name){
 						if($name->season == -1)
 							$namesStrings[] = str_replace("'", "\'",$name->name);
-					}	
+					}
 				}
 			}
 			if(count($namesStrings) == 0)
@@ -421,10 +421,10 @@ class Api extends CI_Controller {
 			echo "'".implode("','", $namesStrings)."',\n";
 		}
 	}
-	
-	
+
+
 	private function _getBestRule($rules, $season=-1, $episode=-1, $ab_episode=-1){
-		
+
 		$bestRule = false;
 		$bestScore = 0;
 		foreach($rules as $curRule){
@@ -437,16 +437,16 @@ class Api extends CI_Controller {
 			if($curScore > $bestScore){
 				$bestScore = $curScore;
 				$bestRule = $curRule;
-			}	
+			}
 		}
 
 		return $bestRule;
 	}
-	
+
 	private function _checkRange($from,$to,$pivot){
 		return (($from <= $pivot && $pivot <= $to)||($from == -1 && $pivot <= $to)||($from <= $pivot && -1 == $to)||($pivot == -1));
 	}
-	
+
 	private function _getScore($from,$to,$pivot){
 		$score = 0;
 		if($from = $pivot && $pivot = $to) // excat match
@@ -456,9 +456,9 @@ class Api extends CI_Controller {
 		if($this->_checkRange($from,$to,$pivot)) // every other match
 			$score += 1;
 		return $score;
-		
+
 	}
-	
-	
-	
+
+
+
 }
