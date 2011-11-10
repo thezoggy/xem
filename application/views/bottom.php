bdfbc6368f5d5c735b434e815db376cb216deeb2
Dennis Lutter <lad1337@gmail.com>
2011-11-10 12:58:15 +0100

    whitespace

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -27,6 +27,7 @@
 </div>
 </div><!-- end #content -->
 </div><!-- end #page -->
+
 <div id="overlay"></div>
 </div><!-- end #everything -->
 <script type="text/javascript">
@@ -34,7 +35,7 @@
 	_gaq.push(['_setAccount', 'UA-22409438-2']);
 	_gaq.push(['_gat._anonymizeIp']);
 	_gaq.push(['_trackPageview']);
-	
+
 	(function() {
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
