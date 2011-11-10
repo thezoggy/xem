37571cd32dc96282c2ef712100c5e1914f78d044
Dennis Lutter <lad1337@gmail.com>
2011-11-10 12:57:55 +0100

    only save when old and new diff

--- application/models/dbobject.php
+++ application/models/dbobject.php
@@ -10,14 +10,14 @@ class DBObject{
 
 	public $id = 0;
 	public $initialData = array();
-	
+
 	function __construct($oh, $dataFields, $id){
 		//print get_class($this)."<br>";
 		$this->oh = $oh;
 		$this->db = $oh->db;
 		$this->cache = $oh->cache;
 		$this->history = $oh->history;
-		
+
 		$this->className = strtolower(get_class($this));
 		$this->table = $this->className."s";
 		$this->dataFields = $dataFields;
@@ -31,9 +31,13 @@ class DBObject{
 		if(!$this->id)
 			$this->load();
 		if($this->id){
-			$this->history->createEvent('update',$this);
-			//print "updating ".$this->className." ".$this->id."</br>";
-			$this->db->update($this->table, $this->buildNameValueArray($this->dataFields), array("id"=>$this->id));
+		    $valueArray = $this->buildNameValueArray($this->dataFields);
+		    $diff = array_diff($valueArray,$this->initialData);
+            if($diff){
+    			$this->history->createEvent('update',$this);
+    			//print "updating ".$this->className." ".$this->id."</br>";
+    			$this->db->update($this->table, $valueArray, array("id"=>$this->id));
+            }
 		}else{
 			$this->history->createEvent('insert',$this);
 			//print "inserting new ".$this->className."... ";
@@ -58,12 +62,12 @@ class DBObject{
 			}
 		}else{
 			if($cachedObj = $this->cache->hasCache(get_class($this),$this->id)){
-			
+
 				//print "loading a ".$this->className." from cache with id: ".$this->id."<br>";
 				$this->setAtributes($cachedObj->buildNameValueArray());
 				return;
 			}else{
-				
+
 				//print "loading a ".$this->className." with id: ".$this->id."<br>";
 				$testRes = $this->db->get_where($this->table, array("id"=>$this->id));
 				if(rows($testRes)){
@@ -74,22 +78,22 @@ class DBObject{
 			$this->initialData = $this->buildNameValueArray();
 		}
 	}
-	
+
 	public function buildNameValueArray($sourceArray=false){
 		if(!$sourceArray)
 			$sourceArray = $this->dataFields;
 		$result = array();
 		foreach($sourceArray as $name){
 			if(isset($this->$name))
-				$result[$name] = $this->$name;	
+				$result[$name] = $this->$name;
 		}
 		return $result;
 	}
-	
+
 	private function setAtributes($array){
 		foreach($array as $name=>$value){
 			$this->$name = $value;
-			
+
 			if(endswith($name, "_id")){
 				$name = explode("_",$name);
 				$name = $name[0];
@@ -99,10 +103,10 @@ class DBObject{
 				$this->$name = new $objName($this->oh, $value);
 				$this->objs[$name] =& $this->$name;
 			}
-			
+
 		}
 	}
-	
+
 	public function __toString(){
     	$out = $this->buildNameValueArray($this->dataFields);
     	$out['id'] = $this->id;
@@ -120,9 +124,9 @@ class DBObject{
     	}
         return str_replace("Array",$this->className."(".$this->id.")",print_r($out, true));
     }
-    
-    
-    
+
+
+
     function delete(){
     	if(!$this->id){
 			$this->load();
