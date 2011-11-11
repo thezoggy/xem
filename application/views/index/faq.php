a9186f53554593d15a5499a539879c1c01ae4cde
Dennis Lutter <lad1337@gmail.com>
2011-11-11 14:44:45 +0100

    little more faq

--- application/views/index/faq.php
+++ application/views/index/faq.php
@@ -1,16 +1,21 @@
 
 <h1>FAQ</h1>
 
+<h3>What does my user Level mean?</h3>
+<p>
+    <ul>
+        <li><label>lvl 0</label><span>Not registerd. you can only look but not touch</span></li>
+        <li><label>lvl 1</label><span>you can log in and can edit most stuff</span></li>
+        <li><label>lvl 2</label><span>you can edit complex shows where the mapping is not easy to understand</span></li>
+        <li><label>lvl 3</label><span>you can revert to revisions, mass edit / delete a show</span></li>
+        <li><label>lvl 4</label><span>you can set the level of other users</span></li>
+        <li><label>lvl 5</label><span>you are an admin you may kick other users with same or lower lvl</span></li>
+    </ul>
+</p>
 
-<h3>User Level</h3>
-<ul>
-<li><label>0</label><span>Not registerd. you can only look but not touch</span></li>
-<li><label>1</label><span>you can log in and can edit most stuff</span></li>
-<li><label>2</label><span>you can edit complex shows where the mapping is not easy to understand</span></li>
-<li><label>3</label><span>you can view history and revert to revisions</span></li>
-<li><label>4</label><span>you can mass edit / delete a show</span></li>
-<li><label>5</label><span>you are an admin</span></li>
-<li><label>6</label><span>you are considered site owner</span></li>
-</ul>
 
-
+<h3>What does the lvl of a show mean?</h3>
+<p>
+its the called the "lock level". you will need at least this level to edit the show.<br/>
+most shows will have a lvl 1 or 2
+</p>
