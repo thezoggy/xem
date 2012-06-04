c51b722be5ce707f689532bd2fbc88b7bf11c617
Jonathon Saine <thezoggy@gmail.com>
2012-06-04 16:27:26 -0500

    Prettify the imprint page..

--- application/views/index/imprint.php
+++ application/views/index/imprint.php
@@ -1,12 +1,46 @@
-<h1>Imprint</h1>
+<h2>Imprint</h2>
 
-<ul>
-<li><label>Admin</label><span>Dennis Lutter</span></li>
-<li><label>Email</label><span><?=safe_mailto('lad1337@gmail.com')?></span></li>
-<li><label>Adress</label><span>Eppendorfer Landstr.166 | 20251 Hamburg</span></li>
-</ul>
+<div class="well">
+    <div class="row-fluid">
+        <span class="span1"><b>Admin</b></span> <span class="span6">Dennis Lutter</span>
+    </div>
+    <div class="row-fluid">
+        <span class="span1"><b>E-Mail</b></span> <span class="span6"><?=safe_mailto('lad1337@gmail.com')?></span>
+    </div>
+    <div class="row-fluid">
+        <span class="span1"><b>Address</b></span> <span class="span6">Eppendorfer Landstr.166 | 20251 Hamburg</span>
+    </div>
+</div>
 
 <h2>Special thanks to:</h2>
-<p>
-zoggy, Tacki, fluffy
-</p>
+
+<div class="row-fluid">
+    <div class="span6">
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>E-Mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>zoggy</td>
+                <td>xxx</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Tacki</td>
+                <td>xxx</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>fluffy</td>
+                <td>xxx</td>
+              </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
