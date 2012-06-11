71be5423b536036267b966e7c385cd6de11323d0
Dennis Lutter <lad1337@gmail.com>
2012-06-11 21:13:31 +0200

    use form-inline class for user config

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -4,7 +4,7 @@
 
 <h3>Change Password</h3>
 
-<?=form_open("user/changePw",array('id'=>'changePw'))?>
+<?=form_open("user/changePw",array('id'=>'changePw', 'class'=>'form-inline'))?>
     <ul>
         <li><label style="width: 160px;" >Current Password</label><input name="old_pw" type="password"/></li>
         <li><label style="width: 160px;" >New Password</label><input name="new_pw" type="password"/></li>
@@ -15,12 +15,13 @@
     <small>Note: Upon changing your password, you must log back in</small>
 </form>
 <?if(grantAcces(4)):?>
+<br/>
 <h3>Get email notifications on</h3>
-<?=form_open("user/emailSettings")?>
+<?=form_open("user/emailSettings",array('class'=>'form-inline'))?>
     <ul>
-        <li><label style="width: 160px;display: inline-block;" >New user activation</label><input name="email_new_account" type="checkbox" <?if($config_email_new_account)echo 'checked="checked"'; ?> value="1"/></li>
-        <li><label style="width: 160px;display: inline-block;" >New show creation</label><input name="email_new_show" type="checkbox" <?if($config_email_new_show)echo 'checked="checked"'; ?> value="1"/></li>
-        <li><label style="width: 160px;display: inline-block;" >Draft public request</label><input name="email_public_request" type="checkbox" <?if($config_email_public_request)echo 'checked="checked"'; ?> value="1"/></li>
+        <li><label style="width: 160px;" >New user activation</label><input name="email_new_account" type="checkbox" <?if($config_email_new_account)echo 'checked="checked"'; ?> value="1"/></li>
+        <li><label style="width: 160px;" >New show creation</label><input name="email_new_show" type="checkbox" <?if($config_email_new_show)echo 'checked="checked"'; ?> value="1"/></li>
+        <li><label style="width: 160px;" >Draft public request</label><input name="email_public_request" type="checkbox" <?if($config_email_public_request)echo 'checked="checked"'; ?> value="1"/></li>
     </ul>
     <input type="submit" value="Save Email Settings" class="btn" />
     <br/>
