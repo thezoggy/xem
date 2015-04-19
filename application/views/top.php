13db73bd3ddb9656d8c021b1f0414bf4b4244680
Jonathon Saine <thezoggy@gmail.com>
2015-04-19 09:51:26 -0500

    Upgrade jquery ui from 1.9.2 (Nov 2012) to 1.10.4 (Jan 2014)  -- Drops IE6 support (which is fine since xem doesnt work on ie7 and older (bootstrap)

--- application/views/top.php
+++ application/views/top.php
@@ -43,12 +43,12 @@ made at http://patorjk.com/software/taag/ with font Georgia11
 -->
 
         <? echo link_tag('css/bootstrap.css', 'stylesheet', 'text/css'); ?>
-        <? echo link_tag('css/smoothness/jquery-ui-1.9.2.custom.css', 'stylesheet', 'text/css'); ?>
+        <? echo link_tag('css/smoothness/jquery-ui-1.10.4.custom.css', 'stylesheet', 'text/css'); ?>
         <? echo link_tag('css/jquery.qtip.min.css', 'stylesheet', 'text/css'); ?>
         <? echo link_tag('css/main.css', 'stylesheet', 'text/css'); ?>
 
         <script type="text/javascript" src="<?php echo base_url();?>js/jquery-1.7.2.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url();?>js/jquery-ui-1.9.2.custom.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url();?>js/jquery-ui-1.10.4.custom.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url();?>js/html5boilerplate.consolewrapper.js"></script>
         <script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url();?>js/jquery.transition.js"></script>
