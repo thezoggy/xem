9d71c340261973659602247220c11d0055a4ab3e
Jonathon Saine <thezoggy@gmail.com>
2015-07-31 09:49:42 -0500

    Remove contact and add proxy in routes, gutted proxy and return response to help educate that there is no more proxy (and so we can stop throwing 404 errors in logs/wasting resources).

--- application/controllers/proxy.php
+++ application/controllers/proxy.php
@@ -1,51 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 class Proxy extends CI_Controller {
 
-	function __construct(){
-		parent::__construct();
+    function __construct() {
+        parent::__construct();
+    }
 
-		$this->load->helper("url");
-		$this->load->model('dbobjectcache');
-		$this->history = new History($this->db,$this->session);
-		$this->dbcache = new DBCache($this->db);
-		$this->oh = new Objectholder($this->db, $this->dbobjectcache, $this->history, $this->dbcache);
+    public function index() {
+        return false;
+    }
+    public function tbdv() {
+        $this->output->set_content_type('application/json')->set_output('you made a typo...');
+        return false;
+    }
+
+	public function tvdb() {
+        $this->output->set_content_type('application/json')->set_output('decommissioned, please stop hitting this ip');
+        return false;
 	}
-
-	public function index(){
-		redirect('xem');
-	}
-
-	public function tvdb(){
-
-	    //print $this->uri->segment(3)."<br>";
-	    //print $this->uri->segment(4)."<br>";
-
-        //$api_regex = '/\/api\/([0-9A-F]+)\/series\/(\d+)\/?(.*)/';
-    	//$result = preg_match($api_regex, current_url(), $matches);
-    	
-    	
-        $segmentSize = $this->uri->total_segments();
-        $segments = $this->uri->segment_array();
-        $newUri = "";
-        for($i=4;$i<=$segmentSize;$i++){
-            $newUri .= "/".$segments[$i];
-        }
-
-    	if($newUri){
-            $proxy = new TvdbProxy($this->oh, $newUri, $this->uri->segment(3));
-            if($proxy->validUrl){
-                $xml = $proxy->call();
-                $this->output->set_output($xml);
-                return;
-            }else{
-                $proxy->passthruCall($newUri);
-            }
-        }
-
-        
-	}
-
 
 }
