224eea038cbbc454c307524fb9ffeb48487dad22
Jonathon Saine <thezoggy@gmail.com>
2015-08-06 18:53:30 -0500

    Use local image for donate button, apply css to match other footer images. Update xem link to my repo.

--- application/views/bottom.php
+++ application/views/bottom.php
@@ -7,11 +7,11 @@
 
     <br>
     <footer class="footer">
-        <p class="pull-right"><a href="#"><img src="<?php echo base_url();?>images/top.gif"></a></p>
+        <p class="pull-right"><a href="#"><img alt="top" src="<?php echo base_url();?>images/top.gif"></a></p>
         <div><strong>{elapsed_time}</strong>s says <?=anchor('http://codeigniter.com/', img( array('src'=>'images/social-codeigniter.png', 'title'=>'CodeIgniter', 'class'=>'social') ));?></div>
-        <div><?=anchor('https://github.com/lad1337/xem', img( array('src'=>'images/social-github.png', 'title'=>'GitHub', 'class'=>'social') ));?></div>
+        <div><?=anchor('https://github.com/thezoggy/xem', img( array('src'=>'images/social-github.png', 'title'=>'GitHub', 'class'=>'social') ));?></div>
         <div><?=anchor('imprint', img( array('src'=>'images/social-imprint.png', 'title'=>'Imprint', 'class'=>'social') ));?></div>
-        <div><?=anchor('http://validator.w3.org/check?uri='.curPageURL(), img( array('src'=>'images/html5_bw_badge22.png', 'title'=>'HTML5 Powered with CSS3 / Styling, Graphics, 3D &amp; Effects, and Semantics', 'class'=>'social') ));?></div>
+        <div><?=anchor('http://validator.w3.org/check?uri='.curPageURL(), img( array('src'=>'images/html5_bw_badge22.png', 'title'=>'Powered by HTML5', 'class'=>'social') ));?></div>
         <div>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_donations">
@@ -21,7 +21,7 @@
                 <input type="hidden" name="no_note" value="0">
                 <input type="hidden" name="currency_code" value="EUR">
                 <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest">
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <input type="image" src="<?php echo base_url();?>images/btn_donate_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!" class="social">
             </form>
         </div>
     </footer>
